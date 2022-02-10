323dbbb85f271b00e4da02db2518e05a923aa019
Julia Leonova <lyus20f145@student.bmstu.ru>
2022-02-10 21:30:00 +0300

    6

--- connection.cpp
+++ connection.cpp
@@ -1,6 +1,55 @@
 #include "connection.h"
+#include "ui_connection.h"
 
-Connection::Connection(QWidget *parent)
-{
+Connection::Connection(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Connection) {
+    ui->setupUi(this);
+}
 
+Connection::~OpenDialog() {
+    delete ui;
+}
+
+QString Connection::get_host() {
+    return ui->HostLineEdit->text();
+}
+
+int Connection::get_port() {
+    return ui->PortLineEdit->text().toInt();
+}
+
+QString Connection::get_DatabaseName() {
+    return ui->DBLineEdit->text();
+}
+
+QString Connection::get_login() {
+    return ui->LoginLineEdit->text();
+}
+
+QString Connection::get_password() {
+    return ui->PasswordLineEdit->text();
+}
+
+void Connection::set_host(const QString& host) {
+    ui->HostLineEdit->insert(host);
+}
+
+void Connection::set_port(int port) {
+    char buffer[128];
+    itoa(port, buffer, 10);
+    QString str(buffer);
+    ui->PortLineEdit->insert(str);
+}
+
+void Connection::set_DatabaseName(const QString& dbName) {
+    ui->DBLineEdit->insert(dbName);
+}
+
+void Connection::set_login(const QString& login) {
+      ui->LoginLineEdit->insert(login);
+}
+
+void Connection::set_password(const QString& password) {
+     ui->PasswordLineEdit->insert(password);
 }
--- connection.h
+++ connection.h
@@ -1,44 +1,31 @@
 #ifndef CONNECTION_H
 #define CONNECTION_H
 
-//#include "StudentDialog.h"
-//#include "TeacherDialog.h"
-
 #include <QDialog>
-//#include <QSqlDatabase>
-#include <QSettings>
 
 namespace Ui {
 class Connection;
 }
 
-struct ConnectionInfo {
-    QString host;
-    QString login;
-    QString password;
-    int port;
-    QString DBName;
-};
-
-class Connection : public QDialog
-{
+class Connection : public QDialog {
     Q_OBJECT
 
 public:
-
     explicit Connection(QWidget *parent = nullptr);
     ~Connection();
-
-private slots:
-
-    void accept();
+    QString get_host();
+    int get_port();
+    QString get_DatabaseName();
+    QString get_login();
+    QString get_password();
+    void set_host(const QString&);
+    void set_port(int);
+    void set_DatabaseName(const QString&);
+    void set_login(const QString&);
+    void set_password(const QString&);
 
 private:
     Ui::Connection *ui;
-    QSettings* connection_settings;
-//    QSqlDatabase DB;
-    void save_ConnectionInfo(const ConnectionInfo&);
-    void load_ConnectionInfo(ConnectionInfo&);
 };
 
 #endif // CONNECTION_H
